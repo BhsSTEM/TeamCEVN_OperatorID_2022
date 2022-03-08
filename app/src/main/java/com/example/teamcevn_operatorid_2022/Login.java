5eddd45bf7cd0230fa124fe4a95d412a842673ec
Cole Ortiz <coleaortiz@gmail.com>
2022-03-08 14:05:09 -0600

    Add log in screen with username and password. Still need to make it so clicking "log in" actually takes you to the main screen.

--- app/src/main/java/com/example/teamcevn_operatorid_2022/Login.java
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/Login.java
@@ -1,17 +1,15 @@
 package com.example.teamcevn_operatorid_2022;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent login = new Intent(this, Login.class);
-        startActivity(login);
+        setContentView(R.layout.login);
     }
-} 
+}
