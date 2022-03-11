afaf00790eadebec20d29c292408b420baedebf1
nrtil <nrtillman@gmail.com>
2022-03-11 13:16:12 -0600

    Machine Settings Screen

--- app/src/main/java/com/example/teamcevn_operatorid_2022/MainActivity.java
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.teamcevn_operatorid_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        Intent intent = new Intent(this, Test.class);
+        startActivity(intent);
     }
 }
