15c72139dbe24f4ccb699448c0993c4f59978bcf
Siddapa <vishnuschalla@gmail.com>
2022-03-30 15:14:57 -0500

    Navigation for all pages (code should be globalized instead of capy pasted though)

--- app/src/main/java/com/example/teamcevn_operatorid_2022/Login.java
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/Login.java
@@ -3,7 +3,9 @@ package com.example.teamcevn_operatorid_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void profile(View view){
+        startActivity(new Intent(this, UserList.class));
+    }
+
+    public void map(View view){
+        // TODO Needs map class
+//        startActivity(new Intent(this, ));
+    }
+
+    public void settings(View view){
+        startActivity(new Intent(this, MachineSettings.class));
     }
 }
--- app/src/main/java/com/example/teamcevn_operatorid_2022/MainActivity.java
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/MainActivity.java
@@ -10,15 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        
-        Intent intent = new Intent(this, Ethan.class);
-        startActivity(intent);
-        
-        // setContentView(R.layout.ethan_main);
-      
-        //Intent intent = new Intent(this, Login.class);
-        //startActivity(intent);
-        
-        //Intent user_list = new Intent(this, UserList.class);
-        //startActivity(user_list);
+        startActivity(new Intent(this, Login.class));
     }
 } 
