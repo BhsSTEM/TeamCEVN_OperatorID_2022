32adadeee75d111a7cfb1ab19355f41e7f1dc5c9
OlderogEthan803 <ejolderog@gmail.com>
2022-04-01 14:34:40 -0500

    fixed gmaps

--- app/src/main/java/com/example/teamcevn_operatorid_2022/MapsActivity.java
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/MapsActivity.java
@@ -45,8 +45,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
     }
 }
