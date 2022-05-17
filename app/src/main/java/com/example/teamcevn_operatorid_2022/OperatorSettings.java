6d578fe0850d65487a376e44a0ec79639e91d4fb
nrtil <nrtillman@gmail.com>
2022-05-17 14:26:56 -0500

    stuff

---
+++ app/src/main/java/com/example/teamcevn_operatorid_2022/OperatorSettings.java
@@ -0,0 +1,40 @@
+package com.example.teamcevn_operatorid_2022;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class OperatorSettings extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    BottomNavigationView nav_bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.operator_settings);
+
+        nav_bar = findViewById(R.id.bottom_navigation);
+        nav_bar.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        CharSequence title = item.getTitle();
+        if ("Profile".contentEquals(title)) {
+            startActivity(new Intent(this, UserList.class));
+        } else if ("Pin".contentEquals(title)) {
+            startActivity(new Intent(this, MapsActivity.class));
+        } else if ("Setting".contentEquals(title)) {
+            startActivity(new Intent(this, MachineSettings.class));
+        }
+        return false;
+    }
+}
